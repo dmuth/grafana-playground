afec86ac10ae3a529c1812158601f07688038cfe
Douglas Muth <dmuth@dmuth.org>
2021-08-31 21:12:23 -0400

    Updated to stop writing to /var/log/, since we can link a single file within /var/log/ in Docker. :-)

--- bin/populate-logs.sh
+++ bin/populate-logs.sh
@@ -6,8 +6,7 @@
 # Errors are fatal
 set -e
 
-LOG_LOCAL=/var/log/for-promtail.log
-LOG_DOCKER=logs/for-dockerized-promtail.log
+LOG=logs/for-dockerized-promtail.log
 CURRENT_USER=${USER}
 
 # Change to parent directory of this script
@@ -21,7 +20,7 @@ function print_syntax() {
 	echo "! "
 	echo "! Syntax: $0 [ truncate ] "
 	echo "! "
-	echo "! truncate - If specified, truncate the logs."
+	echo "! truncate - If specified, truncate the log."
 	echo "! "
 	exit 1
 }
@@ -36,13 +35,11 @@ fi
 if test "$1" == "truncate"
 then
 	echo "# "
-	echo "# Truncating logfiles: "
+	echo "# Truncating logfile: "
 	echo "# "
-	echo "# - ${LOG_LOCAL}"
-	echo "# - ${LOG_DOCKER}"
+	echo "# - ${LOG}"
 	echo "# "
-	echo > ${LOG_LOCAL} 
-	echo > ${LOG_DOCKER}
+	echo > ${LOG} 
 	echo "# Logs truncated!"
 	exit
 
@@ -54,22 +51,9 @@ fi
 
 
 echo "# "
-echo "# Creating ${LOG_LOCAL} and changing owner to ${CURRENT_USER}..."
-echo "# You may be asked for your sudo password shortly..."
+echo "# Now writing regular lines to this file: "
 echo "# "
-sudo touch ${LOG_LOCAL}
-sudo chown ${CURRENT_USER} ${LOG_LOCAL}
-
-echo "# Okay, ${LOG_LOCAL} exists and is now owned by you!"
-
-echo "# Making log directory for ${LOG_DOCKER}..."
-mkdir -p $(dirname ${LOG_DOCKER})
-
-echo "# "
-echo "# Now writing regular lines to each of these files: "
-echo "# "
-echo "# - ${LOG_LOCAL}"
-echo "# - ${LOG_DOCKER}"
+echo "# - ${LOG}"
 echo "# "
 echo "# Press ctrl-C to stop at anytime..."
 echo "# "
@@ -78,8 +62,7 @@ NUM=0
 while true
 do
 	LINE="$(date) ${HOSTNAME} synthetic_data=true, message=${NUM}"
-	echo $LINE >> ${LOG_LOCAL}
-	echo $LINE >> ${LOG_DOCKER}
+	echo $LINE >> ${LOG}
 	NUM=$(( NUM += 1 ))
 	sleep 1
 done
