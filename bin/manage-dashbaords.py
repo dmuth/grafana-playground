7e5c7885cdb7213d7163b4242214b5c012041b1a
Douglas Muth <dmuth@dmuth.org>
2021-09-23 21:48:52 -0400

    Added import functionality!

--- bin/manage-dashbaords.py
+++ bin/manage-dashbaords.py
@@ -6,6 +6,7 @@
 
 import argparse
 import json
+import sys
 
 import requests
 
@@ -90,6 +91,82 @@ def export(url, api_key):
 
 	return(dashboards)
 
+
+#
+# Wrapper to print a string to stderr.
+#
+def stderr(string):
+		print(string, file=sys.stderr)
+
+
+#
+# Read JSON from stdin and parse it.
+#
+def import_json():
+
+	string = ""
+	for line in sys.stdin:
+		string += line
+
+	data = json.loads(string)
+
+	#
+	# Check to see if we have the right meta data.
+	# This is mainly a sanity check because I don't want random data being fed
+	# into this and then bug reports getting triggered.
+	# I *only* want data that was created by this script's export process. :-)
+	#
+	if "meta" in data:
+		if "app" in data["meta"]:
+			if data["meta"]["app"] == "Grafana Playground":
+				return(data["dashboards"])
+			else:
+				raise Exception(f"Invalid app type '{data['meta']['app']}' in metadata, expecting 'Grafana Playground'!")
+		else:
+			raise Exception("Invalid metadata in export file.")
+	else:
+		raise Exception("Metadata not found in export file.")
+
+
+#
+# Go through our dashboards and import them.
+#
+def import_dashboards(url, api_key, dashboards):
+
+	headers = {
+    "Authorization": f"Bearer {api_key}",
+		"Content-Type":"application/json",
+		"Accept": "application/json"
+	}
+	url_import = f"{url}/api/dashboards/db"
+
+	for dashboard in dashboards:
+		new_dashboard = {"dashboard": dashboard}
+		id = new_dashboard["dashboard"]["id"]
+		uid = new_dashboard["dashboard"]["uid"]
+		title = new_dashboard["dashboard"]["title"]
+		new_dashboard["overwrite"] = True
+
+		r = requests.post(url = url_import, headers = headers, 
+			data = json.dumps(new_dashboard))
+		stderr(f"Importing dashboard uid={uid} id={id} title={title}...")
+
+		if r.status_code == 404:
+			stderr(f"Dashboard uid {uid} not found, let's create it instead!")
+			new_dashboard["overwrite"] = False
+			new_dashboard["dashboard"]["id"] = None
+
+			r = requests.post(url = url_import, headers = headers, 
+				data = json.dumps(new_dashboard))
+
+		elif r.status_code != 200:
+			raise Exception(f"Status code {r.status_code} != 200 for URL '{url_import}'!  Message returned: {r.text}")
+
+		stderr(f"Imported dashboard '{new_dashboard['dashboard']['title']}' " + 
+				f"(uid {uid}), "
+				+ f"results: {r.text}")
+
+
 #
 # Our main entrypoint.
 #
@@ -107,7 +184,14 @@ def main():
 		output["meta"] = {"app": "Grafana Playground"}
 		print(json.dumps(output))
 
-	# TODO: args.import should read JSON from stdin
+	elif args.__dict__["import"]:
+		# import is a great term, but a reserved word, hence messing with __dict__...
+		stderr("Now reading JSON of an export file from stdin...")
+		dashboards = import_json()
+		import_dashboards(url, args.api_key, dashboards)
+
+	else:
+		raise("Invalid arguments!")
 
 
 main()
